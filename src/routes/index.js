5a05d1c14fcae4e27224e185b7ee4e60a68c92be
julio olguin <julioolguin8@gmail.com>
2020-04-10 17:39:53 -0500

    ajuste de home

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,7 +6,7 @@ import getHash from '../utils/getHash';
 import resolveRoutes from '../utils/resolveRoutes';
 
 const routes = {
-  'https://jc-olguins.github.io/curso-spa/': Home,
+  '/': Home,
   '/:id': Character,
   '/contact': 'Contact',
 };
